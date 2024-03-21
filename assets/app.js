22f7f82b7922602c573cb56e49d4e8bf8392e2b5
Barış Kahraman <baris-kahra@hotmail.com>
2024-03-21 19:09:46 +0300

    Kullanıcıdan veri alımı için oluşturulan butonlar js tarafında kullanıldı. Öncesinde oluşturulan algoritma alıştırmaları kullanıcı ile etkileşimde bulunarak çalışması için gerekli html css ve js kodları yazıldı.

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,5 @@
 const accordion = document.querySelectorAll(".accordion");
+const forms = document.querySelectorAll('form');
 
 for (const acc of accordion) {
     acc.addEventListener("click", function() {
@@ -12,29 +13,63 @@ for (const acc of accordion) {
   });
 }
 
-// Dizideki İkili Toplamlar:
-const array = [1,2,3,4,5,6,7,8,9,10];
-console.log(`Dizinin elemanları şunlardır: ${array}`);
-for(let i = 0 ; i < array.length ; i++){
-    for(let j = 0 ; j < array.length ; j++){
-        if( i + j == 10){
-            console.log(`${i} + ${j} = ${i + j}`);
+for (const form of forms) {
+    form.addEventListener('submit', function(e){
+        e.preventDefault();
+        const inputElement = document.querySelector(`#${e.target.id} input`);
+        if(inputElement.name == "dit"){
+            return sumPairs(inputElement.value);
+        }else if(inputElement.name == "fs"){
+            return fibSeries(inputElement.value);
+        }
+        
+    })
+    
+}
+
+// Dizideki ikili toplamlar
+function sumPairs(array){
+    const ditDiv = document.querySelector('#dit-div');
+    const fsTotal = document.querySelector('input[name="dit-total"]');
+    let arr = array.split(',');
+    let trimmedArr = arr.map(s => s.trim());
+    console.log(trimmedArr)
+    for (let i = 0 ; i < arr.length ; i++) {
+        for(let j = i+1 ; j < arr.length ; j++){
+            if(Number(arr[i]) == Number(arr[j])){
+                return ditDiv.innerHTML += `<div class="result">Lütfen birbirinden farklı sayılar girin.</div>`
+            }
+        }
+    }
+    ditDiv.innerHTML += `<h3>Dizinin elemanları şunlardır: ${trimmedArr.map(s => ` ${s} `)}</h3>`;
+    for(let i = 0 ; i < trimmedArr.length ; i++){
+        for(let j = 0 ; j < trimmedArr.length ; j++){
+            if( Number(trimmedArr[i]) + Number(trimmedArr[j]) == Number(fsTotal.value)){
+                ditDiv.innerHTML += `<div class="result">${trimmedArr[i]} + ${trimmedArr[j]} = ${Number(trimmedArr[i]) + Number(trimmedArr[j])} <br></div>`
+            }
         }
     }
 }
 
+
 // Fibonacci Serisi:
-let fib = []
-let num1 = 1;
-let num2 = 1;
 
-for(let i = 0 ; i < 10 ; i++){
-    let total = num1 + num2;
-    console.log(`Fibonacci Toplamları : ${num1} + ${num2} = ${total}`);
-    num1 = num2;
-    num2 = total;
-    fib.push(num1);
-
+function fibSeries(val){
+    const fsDiv = document.querySelector('#fs-div');
+    let fib = [];
+    let num1 = 1;
+    let num2 = 1;
+    fsDiv.innerHTML += `<h3>Fibonacci Toplamları : </h3>`
+    for(let i = 0 ; i < val ; i++){
+        let total = num1 + num2;
+        fsDiv.innerHTML += ` ${num1} + ${num2} = ${total} <br>`;
+        num1 = num2;
+        num2 = total;
+        fib.push(num1);
+        if(i == val - 1 ){
+            fib.push(total);
+        }
+    }
+    fsDiv.innerHTML += `Fibonacci Serisi: ${fib}`;
 }
-console.log(`Fibonacci Serisi: ${fib}`);
 
